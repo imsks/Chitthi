8c9149454b1377612cb9e8a032a757b5e3b6edad
Sachin <sachinkshuklaoo7@gmail.com>
2025-06-08 23:09:50 +0530

    Add email sending functionality with request validation

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/imsks/chitthi/internal/config"
+	"github.com/imsks/chitthi/internal/model"
 )
 
 func main() {
@@ -15,7 +17,31 @@ func main() {
 		fmt.Println(w, "📮 Welcome to Chitthi - BYOK Email Delivery Service")
 	})
 
+	http.HandleFunc("/send-email", handleSendEmail)
+
 	addr := ":" + cfg.Port
 	fmt.Printf("🚀 Chitthi running on http://localhost%s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func handleSendEmail(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	var req model.EmailRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if req.To == "" || req.Subject == "" || req.Provider == "" {
+		http.Error(w, "Missig required fields", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("📨 Received Email Request: %+v", req)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("Email accepted for processing."))
 }
